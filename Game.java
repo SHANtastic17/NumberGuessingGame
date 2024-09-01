9d41275c6d0978e3291e412d1ac3969cfd7418c8
SHANtastic17 <148371535+SHANtastic17@users.noreply.github.com>
2024-09-01 19:17:59 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -10,80 +10,161 @@ import java.util.concurrent.TimeoutException;
 
 public class Game {
     public Game() {
+        // Constructor body can be empty or contain initialization code
     }
 
-    boolean guessNumber(final Level var1) throws InterruptedException {
-        int var2 = (new Random()).nextInt(var1.max - var1.min + 1) + var1.min;
+    static class GameResult {
+        boolean won;
+        int secretNumber;
+        boolean timedOut;
 
-        while(true) {
-            ExecutorService var3 = Executors.newSingleThreadExecutor();
-            Future var4 = var3.submit(new Callable<Integer>(this) {
+        GameResult(boolean won, int secretNumber, boolean timedOut) {
+            this.won = won;
+            this.secretNumber = secretNumber;
+            this.timedOut = timedOut;
+        }
+    }
+
+    GameResult guessNumber(final Level level, Scanner scanner) throws InterruptedException {
+        int secretNumber = (new Random()).nextInt(level.max - level.min + 1) + level.min;
+
+        while (true) {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Future<Integer> future = executor.submit(new Callable<Integer>() {
                 public Integer call() throws Exception {
-                    Scanner var1x = new Scanner(System.in);
-                    System.out.printf("Take a guess (between %d and %d): ", var1.min, var1.max);
-                    return var1x.nextInt();
+                    System.out.printf("Take a guess (between %d and %d): ", level.min, level.max);
+                    return scanner.nextInt();
                 }
             });
 
             try {
-                int var5 = (Integer)var4.get((long)var1.timeLimit, TimeUnit.SECONDS);
-                if (var5 < var1.min || var5 > var1.max) {
-                    System.out.println("Your guess is out of range. The secret number was " + var2 + ". Please guess a number between " + var1.min + " and " + var1.max + " in your next turn.");
-                    System.exit(0);
+                int guess = future.get(level.timeLimit, TimeUnit.SECONDS);
+                if (guess < level.min || guess > level.max) {
+                    System.out.println("Your guess is out of range. The secret number was " + secretNumber + ". Please guess a number between " + level.min + " and " + level.max + " in your next turn.");
+                    return new GameResult(false, secretNumber, false);
                 }
 
-                if (var5 == var2) {
+                if (guess == secretNumber) {
                     System.out.println("Congratulations! You guessed the number.");
-                    boolean var6 = true;
-                    return var6;
+                    return new GameResult(true, secretNumber, false);
                 }
 
-                if (var1.hints) {
-                    if (Math.abs(var5 - var2) <= 2) {
-                        System.out.println("Near to the number! Try again.");
-                    } else if (Math.abs(var5 - var2) <= 5) {
-                        if (var5 < var2) {
-                            System.out.println("Low! Try again.");
+                if (level.hints) {
+                    int difference = Math.abs(guess - secretNumber);
+                    int range = level.max - level.min;
+
+                    if (level == Level.EASY) {
+                        if (difference <= 1) {
+                            System.out.println("Very close! Try again.");
+                        } else if (difference <= 2) {
+                            System.out.println("Close! Try again.");
+                        } else if (difference <= 5) {
+                            if (guess < secretNumber) {
+                                System.out.println("A bit low! Try again.");
+                            } else {
+                                System.out.println("A bit high! Try again.");
+                            }
+                        } else if (difference > 4) {
+                            if (guess < secretNumber) {
+                                System.out.println("Too low! Try again.");
+                            } else {
+                                System.out.println("Too high! Try again.");
+                            }
                         } else {
-                            System.out.println("High! Try again.");
+                            if (guess < secretNumber) {
+                                System.out.println("Low! Try again.");
+                            } else {
+                                System.out.println("High! Try again.");
+                            }
                         }
-                    } else if (var5 < var2) {
-                        System.out.println("Too low! Try again.");
-                    } else {
-                        System.out.println("Too high! Try again.");
+                    } else if (level == Level.MEDIUM) {
+                        if (difference <= 2) {
+                            System.out.println("Very close! Try again.");
+                        } else if (difference <= 5) {
+                            System.out.println("Close! Try again.");
+                        } else if (difference <= 10) {
+                            if (guess < secretNumber) {
+                                System.out.println("A bit low! Try again.");
+                            } else {
+                                System.out.println("A bit high! Try again.");
+                            }
+                        } else if (difference > 8) {
+                            if (guess < secretNumber) {
+                                System.out.println("Too low! Try again.");
+                            } else {
+                                System.out.println("Too high! Try again.");
+                            }
+                        } else {
+                            if (guess < secretNumber) {
+                                System.out.println("Low! Try again.");
+                            } else {
+                                System.out.println("High! Try again.");
+                            }
+                        }
+                    } else if (level == Level.HARD) {
+                        if (difference <= range * 0.005) { // within 0.5% of the range
+                            System.out.println("Very close! Try again.");
+                        } else if (difference <= range * 0.01) { // within 1% of the range
+                            System.out.println("Close! Try again.");
+                        } else if (difference <= range * 0.025) { // within 2.5% of the range
+                            if (guess < secretNumber) {
+                                System.out.println("A bit low! Try again.");
+                            } else {
+                                System.out.println("A bit high! Try again.");
+                            }
+                        } else if (difference > range * 0.08) { // more than 8% of the range
+                            if (guess < secretNumber) {
+                                System.out.println("Too low! Try again.");
+                            } else {
+                                System.out.println("Too high! Try again.");
+                            }
+                        } else {
+                            if (guess < secretNumber) {
+                                System.out.println("Low! Try again.");
+                            } else {
+                                System.out.println("High! Try again.");
+                            }
+                        }
                     }
                 } else {
                     System.out.println("Try again.");
                 }
-            } catch (TimeoutException var11) {
-                System.out.println("Time's up! You didn't guess in time. The secret number was " + var2 + ".");
-                System.exit(0);
-            } catch (ExecutionException var12) {
-                var12.printStackTrace();
+            } catch (TimeoutException e) {
+                System.out.println();
+                System.out.println("Time's up! You didn't guess in time. The secret number was " + secretNumber + ".");
+                System.exit(0); // Exit the program when time is up
+            } catch (ExecutionException e) {
+                System.out.println("An error occurred while processing your guess.");
+                e.printStackTrace();
+                return new GameResult(false, secretNumber, false);
             } finally {
-                var3.shutdownNow();
+                executor.shutdownNow();
             }
         }
     }
 
-    Level chooseLevel() {
-        Scanner var1 = new Scanner(System.in);
+    Level chooseLevel(Scanner scanner) {
         printWithTypingEffect("Choose a level: 1 for EASY, 2 for MEDIUM, 3 for HARD");
-        int var2 = var1.nextInt();
-        switch (var2) {
-            case 1 -> return Game.Level.EASY;
-            case 2 -> return Game.Level.MEDIUM;
-            case 3 -> return Game.Level.HARD;
-            default -> return Game.Level.EASY;
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character left by nextInt()
+        switch (choice) {
+            case 1:
+                return Game.Level.EASY;
+            case 2:
+                return Game.Level.MEDIUM;
+            case 3:
+                return Game.Level.HARD;
+            default:
+                return Game.Level.EASY;
         }
     }
 
-    public static void main(String[] var0) {
-        String var1 = "NumberWizard";
-        String var2 = "SHANTANU PRADHAN";
-        printWithTypingEffect("Welcome to " + var1 + "!");
+    public static void main(String[] args) {
+        String gameName = "NumberWizard";
+        String designerName = "SHANTANU PRADHAN";
+        printWithTypingEffect("Welcome to " + gameName + "!");
         System.out.println();
-        printWithTypingEffect("Designed by: " + var2);
+        printWithTypingEffect("Designed by: " + designerName);
         printWithTypingEffect("CSE undergrad at SRM-IST");
         System.out.println();
         printWithTypingEffect("Rules:");
@@ -97,46 +178,41 @@ public class Game {
         printWithTypingEffect("4. You will get hints if you are near to the number.");
         System.out.println();
         printWithTypingEffect("Press any key to start the game...");
-        Scanner var3 = new Scanner(System.in);
-        var3.nextLine();
-        Game var4 = new Game();
-        boolean var5 = true;
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        Game game = new Game();
+        boolean playAgain = true;
 
-        while(var5) {
-            Level var6 = var4.chooseLevel();
+        while (playAgain) {
+            Level level = game.chooseLevel(scanner);
 
             try {
-                boolean var7 = var4.guessNumber(var6);
-                if (var7) {
-                    printWithTypingEffect("Do you want to play again? (yes/no)");
-                    Scanner var8 = new Scanner(System.in);
-                    String var9 = var8.nextLine();
-                    var5 = var9.equalsIgnoreCase("yes");
-                } else {
-                    var5 = false;
+                GameResult result = game.guessNumber(level, scanner);
+                if (!result.won && !result.timedOut) {
+                    System.out.println("You didn't guess the number. The secret number was " + result.secretNumber + ".");
                 }
-            } catch (InterruptedException var10) {
-                var10.printStackTrace();
+            } catch (InterruptedException e) {
+                System.out.println("An error occurred during the game.");
+                e.printStackTrace();
             }
-        }
 
+            printWithTypingEffect("Do you want to play again? (yes/no)");
+            scanner.nextLine(); // Consume the newline character left by previous input
+            String response = scanner.nextLine();
+            playAgain = response.equalsIgnoreCase("yes");
+        }
     }
 
-    private static void printWithTypingEffect(String var0) {
-        char[] var1 = var0.toCharArray();
-        int var2 = var1.length;
-
-        for(int var3 = 0; var3 < var2; ++var3) {
-            char var4 = var1[var3];
-            System.out.print(var4);
-
+    private static void printWithTypingEffect(String message) {
+        char[] chars = message.toCharArray();
+        for (char c : chars) {
+            System.out.print(c);
             try {
                 Thread.sleep(60L);
-            } catch (InterruptedException var6) {
-                var6.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-
         System.out.println();
     }
 
@@ -150,11 +226,11 @@ public class Game {
         final int timeLimit;
         final boolean hints;
 
-        private Level(int var3, int var4, int var5, boolean var6) {
-            this.min = var3;
-            this.max = var4;
-            this.timeLimit = var5;
-            this.hints = var6;
+        private Level(int min, int max, int timeLimit, boolean hints) {
+            this.min = min;
+            this.max = max;
+            this.timeLimit = timeLimit;
+            this.hints = hints;
         }
     }
 }
